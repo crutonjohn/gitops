af3c5b4d88cb62279d886404e13361f275d3282f
Curtis John <admin@curtisrayjohn.com>
2020-10-18 21:38:08 -0400

    add blocky to cluster

--- setup/bootstrap-secrets.sh
+++ setup/bootstrap-secrets.sh
@@ -235,6 +235,7 @@ generateSecrets(){
   ksealhelm "${MANIFEST_ROOT}/auth-system/oauth-proxy/oauth-proxy.txt"
   ksealhelm "${MANIFEST_ROOT}/monitoring/prometheus-operator/kube-prometheus-stack-helm-values.txt"
   ksealhelm "${MANIFEST_ROOT}/games/minecraft/minecraft-helm-values.txt"
+  ksealhelm "${MANIFEST_ROOT}/network-system/blocky/blocky-helm-values.txt"
 
   bigMessage "Creating Raw Kubernetes Secrets"
 
