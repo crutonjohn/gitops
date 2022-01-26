e65310b787d6e86e1641dd914698cb97f8002ef1
Curtis John <admin@curtisrayjohn.com>
2022-01-26 18:56:17 -0500

    Deployed d7bd82c with MkDocs version: 1.2.3

---
+++ assets/javascripts/expert-rec-search.js
@@ -0,0 +1,9 @@
+(function() {
+var id = 'b8df7ea0-38a5-11eb-9b20-0242ac130002';
+var ci_search = document.createElement('script');
+ci_search.type = 'text/javascript';
+ci_search.async = true;
+ci_search.src = 'https://cse.expertrec.com/api/js/ci_common.js?id=' + id;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ci_search, s);
+})();
