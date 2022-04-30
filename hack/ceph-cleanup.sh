e3f2832e8804453c49ce9236bda332ee764ce0b7
Curtis John <admin@curtisrayjohn.com>
2022-04-29 21:27:58 -0400

    update ceph chart

--- hack/ceph-cleanup.sh
+++ hack/ceph-cleanup.sh
@@ -4,6 +4,15 @@ echo "Cleaning up after rook-ceph"
 for i in $(seq 1 5);
 do
     echo "Cleaning k-node${i}"
-    ssh root@192.168.130.5${i} ./ceph-cleanup.sh
+    echo ssh root@192.168.130.5${i} ./ceph-cleanup.sh
     sleep 3
+    cat <<EOF
+    ################################
+    #### REBOOTING k-node${i}      ####
+    #### PRESS CTRL+C TO ABORT  ####
+    #### IN THE NEXT 10 SECONDS ####
+    ################################
+EOF
+    sleep 10
+    ssh root@192.168.130.5${i} reboot now
 done
