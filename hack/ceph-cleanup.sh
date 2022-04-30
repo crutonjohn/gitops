b141a81f6a2491d8ef12bcceed702d60f3d3b527
Curtis John <admin@curtisrayjohn.com>
2022-04-29 21:30:47 -0400

    made and oopsie

--- hack/ceph-cleanup.sh
+++ hack/ceph-cleanup.sh
@@ -4,7 +4,7 @@ echo "Cleaning up after rook-ceph"
 for i in $(seq 1 5);
 do
     echo "Cleaning k-node${i}"
-    echo ssh root@192.168.130.5${i} ./ceph-cleanup.sh
+    ssh root@192.168.130.5${i} ./ceph-cleanup.sh
     sleep 3
     cat <<EOF
     ################################
