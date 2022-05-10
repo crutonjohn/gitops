a7e5c6f205c5493bc87d5d996cebca6738ee1940
Curtis John <admin@curtisrayjohn.com>
2022-05-10 16:42:59 -0400

    refactor: migrate to linkerd

--- hack/delete-ns.sh
+++ hack/delete-ns.sh
@@ -3,4 +3,5 @@
 kubectl proxy &
 kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
 curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+rm -f temp.json
 )
