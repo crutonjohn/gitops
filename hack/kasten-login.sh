8a4fa5f2125f1e776738d4fdcfa467765c216dac
Curtis John <admin@curtisrayjohn.com>
2023-07-02 22:05:17 -0400

    adding some stuff for bootstrapping

--- hack/kasten-login.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# get secret name containing token
-sa_secret=$(kubectl get serviceaccount k10-k10 -o jsonpath="{.secrets[0].name}" --namespace kasten-io)
-
-# extract token
-kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\n'}" | base64 --decode
--- hack/rook-password.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo
