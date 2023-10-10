87a899ca8ee49b8a392803d8016796daf221e6de
Curtis John <admin@curtisrayjohn.com>
2023-10-09 22:15:09 -0400

    nixify requirements

--- default.nix
+++ default.nix
@@ -1,23 +1,9 @@
 # shell.nix
 { pkgs ? import <nixpkgs> {} }:
-let
-  my-python-packages = p: with p; [
-    ansible-core
-    ansible
-    #(
-    #  buildPythonPackage rec {
-    #    pname = "python-gilt";
-    #    version = "1.2.3";
-    #    src = fetchPypi {
-    #      inherit pname version;
-    #      sha256 = "sha256-0aozmQ4Eb5zL4rtNHSFjEynfObUkYlid1PgMDVmRkwY=";
-    #    };
-    #    doCheck = false;
-    #    propagatedBuildInputs = [
-    #      # Specify dependencies
-    #    ];
-    #  }
-    #)
-  ];
-  my-python = pkgs.python3.withPackages my-python-packages;
-in my-python.env
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+      cilium-cli
+      k0sctl
+    ];
+}
