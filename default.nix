2eb070d3815498ef6aa9d8acfe5c736f095703de
Curtis John <admin@curtisrayjohn.com>
2026-01-22 14:03:30 -0500

    chore(nix): remove old/unused packages

--- default.nix
+++ default.nix
@@ -1,16 +1,13 @@
 # shell.nix
 
 let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  pkgs = import <nixpkgs> {};
-  unstable = import unstableTarball {};
+  unstableTarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+  pkgs = import <nixpkgs> { };
+  unstable = import unstableTarball { };
 
   shell = pkgs.mkShell {
     buildInputs = [
       pkgs.cilium-cli
-      pkgs.k0sctl
       pkgs.android-tools
       pkgs.envsubst
       pkgs.ceph
@@ -18,9 +15,9 @@ let
       pkgs.pre-commit
       pkgs.go-task
       pkgs.minijinja
-      pkgs.restic
       unstable.talosctl
       unstable.fluxcd
     ];
   };
-in shell
+in
+shell
